727fda0a66ebc9d5deebebbf0e86b49bfdbe79a5
Ilija Hadzic <ih2435@columbia.edu>
2024-04-05 23:16:24 -0400

    labs: add Python build files
    Signed-off-by: Ilija Hadzic <ih2435@columbia.edu>

---
+++ labs/setup.py
@@ -0,0 +1,22 @@
+## ! DO NOT MANUALLY INVOKE THIS setup.py, USE CATKIN INSTEAD
+
+import sys
+from distutils.core import setup
+from catkin_pkg.python_setup import generate_distutils_setup
+
+def read(fname):
+    return open(fname).read()
+
+# fetch values from package.xml
+setup_args = generate_distutils_setup(
+    packages=[],
+    package_dir={'': 'src'},
+    platforms=['ROS'],
+    long_description="Python nodes for Probabilistic Robotics lab"
+)
+
+# overwrite files in dest dir if timestamp later
+if 'install' in sys.argv:
+    sys.argv.append('--force')
+
+setup(**setup_args)
