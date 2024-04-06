c509f5e7b55ce4c0cb4e9802b9bd8db051c309d1
Ilija Hadzic <ih2435@columbia.edu>
2024-04-05 23:58:53 -0400

    door: implement feature processor node
    The node takes the list of features, calculates the mean
    of x coordinates and publishes to /feature_mean topic.
    
    Signed-off-by: Ilija Hadzic <ih2435@columbia.edu>

--- labs/src/image_mean_feature_x/image_mean_feature_x.py
+++ labs/src/image_mean_feature_x/image_mean_feature_x.py
@@ -1,12 +1,35 @@
 #!/usr/bin/env python
 
 import rospy
+import threading
+import numpy
 
+from std_msgs.msg import Float64
+from opencv_apps.msg import Point2DArrayStamped
+
+class ProcessFeatures():
+    def __init__(self):
+        self.feature_sub = rospy.Subscriber(
+            '/goodfeature_track/corners', Point2DArrayStamped,
+            self.handle_features, queue_size = 1)
+        self.mean_pub = rospy.Publisher(
+            'feature_mean', Float64, queue_size = 1)
+        self.lock = threading.Lock()
+
+    def handle_features(self, f):
+        with self.lock:
+            x_features = [ p.x for p in f.points ]
+            mean_x = numpy.mean(x_features)
+            self.mean_pub.publish(mean_x)
+
+    def spin(self):
+        rospy.spin()
 
 def main():
     rospy.init_node('image_mean_feature_x')
     rospy.loginfo('starting image_mean_feature_x')
-    rospy.spin()
+    pf = ProcessFeatures()
+    pf.spin()
     rospy.loginfo('done')
 
 if __name__=='__main__':
