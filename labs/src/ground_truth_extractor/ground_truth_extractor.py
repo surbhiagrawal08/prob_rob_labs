cb8d814ba07485fbb9d880ba8781eb6f8ac665f6
Surbhi Agrawal <sa4480@columbia.edu>
2024-10-24 18:35:25 -0400

    lab 4 assignment 1 wrote ground truth extractor that subscribes to gazebo ground truth and publishes with a time stamp

---
+++ labs/src/ground_truth_extractor/ground_truth_extractor.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+
+import rospy
+from gazebo_msgs.msg import LinkStates
+from geometry_msgs.msg import PoseStamped, TwistStamped
+
+class GroundTruthExtractor:
+    def __init__(self):
+        self.sub_gaz = rospy.Subscriber('/gazebo/link_states', LinkStates, self.ground_truth_callback,
+                                        queue_size=10)
+        self.pub_gt_twist = rospy.Publisher('/jackal/ground_truth/twist', TwistStamped, 
+                                            queue_size = 10)
+        self.pub_gt_pose = rospy.Publisher('/jackal/ground_truth/pose', PoseStamped,
+                                           queue_size = 10)        
+
+    def ground_truth_callback(self, link_states):
+        self.link_names = link_states.name
+        base_index = self.link_names.index("jackal::base_link")
+        now = rospy.get_rostime()
+        gt_twist = TwistStamped()
+        gt_pose = PoseStamped()
+
+        gt_twist.header.stamp, gt_pose.header.stamp = now, now
+        gt_twist.header.frame_id = "jackal::base_link"
+        gt_pose.header.frame_id = "ground_plane::link"
+
+        gt_twist.twist = link_states.twist[base_index]
+        gt_pose.pose = link_states.pose[base_index]        
+
+        self.pub_gt_pose.publish(gt_pose)
+        self.pub_gt_twist.publish(gt_twist)
+        
+def main():
+    rospy.init_node('ground_truth_extractor')
+    rospy.loginfo('starting ground_truth_extractor')
+    ground_truth_extractor = GroundTruthExtractor()
+    rospy.spin()
+    rospy.loginfo('done')
+
+if __name__=='__main__':
+    main()
